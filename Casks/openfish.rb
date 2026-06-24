598f1ef25431fdfbe4dc1af3e7614c4e54d44692
ruben-- <2705752+ruben--@users.noreply.github.com>
2026-06-24 13:55:47 +0200

    openfish 0.1.4

--- Casks/openfish.rb
+++ Casks/openfish.rb
@@ -1,6 +1,6 @@
 cask "openfish" do
-  version "0.1.3"
-  sha256 "2ed5d787036afefb6c05c887c599df2f2a909d658284c3968d25369c37364973"
+  version "0.1.4"
+  sha256 "9dbc7f537dcfb549944f22d8ef0e7160c03879cbec10b61663109dcc406f7183"
 
   url "https://github.com/openfish-sh/openfish/releases/download/v#{version}/Openfish.dmg"
   name "Openfish"
